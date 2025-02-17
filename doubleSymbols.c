c8531d0fb86f867fe64e3a06650b276d9aa9ed19
นาย คชภัค เเก้วรุณา เลขที่34 เลขที่ใหม่ <47728@kalasinpit.ac.th>
2025-02-17 15:29:17 +0700

    Add files via upload

--- doubleSymbols.c
+++ doubleSymbols.c
@@ -1,25 +1,20 @@
+#include <stdio.h>
+
 int main() {
     int n;
 
-    // รับค่าตัวเลข
-    printf("Enter an even number: ");
     scanf("%d", &n);
 
-    // ตรวจสอบว่าเป็นเลขคู่
-    if (n % 2 == 0) {
-        // แสดงเครื่องหมาย * และ + สลับกัน
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                printf("*");
-            } else {
-                printf("+");
-            }
+
+    if (n % 2 == 0 && n > 0) {
+        for (int i = 0; i < n / 2; i++) {
+            printf("*");
+            printf("+");
         }
         printf("\n");
     } else {
-        // หากไม่เป็นเลขคู่
         printf("Wrong input\n");
-    }
 
-    return 0;
+     }
+  return 0;
 }
